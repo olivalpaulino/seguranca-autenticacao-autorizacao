14d81235a6c2cf16d2bdc11203579ddce186c5c9
Olival Paulino <olivalpaulinocc@gmail.com>
2025-05-24 16:32:43 -0300

    adicionando a entidade Role.java

--- src/main/java/entity/Role.java
+++ src/main/java/entity/Role.java
@@ -1,4 +1,36 @@
 package entity;
 
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    // Mapeamento inverso opcional:
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    // Construtores
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    // Getters e Setters ...
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Set<User> getUsers() { return users; }
+    public void setUsers(Set<User> users) { this.users = users; }
 }
