0d61f7513f4e54c5bb71f8660540c3f3e9ee6225
Olival Paulino <olivalpaulinocc@gmail.com>
2025-05-24 15:17:46 -0300

    criando projeto

---
+++ src/main/java/dobackaofront/seguranca_autenticacao_autorizacao/SegurancaAutenticacaoAutorizacaoApplication.java
@@ -0,0 +1,13 @@
+package dobackaofront.seguranca_autenticacao_autorizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancaAutenticacaoAutorizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurancaAutenticacaoAutorizacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dobackaofront/seguranca_autenticacao_autorizacao/SegurancaAutenticacaoAutorizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dobackaofront.seguranca_autenticacao_autorizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurancaAutenticacaoAutorizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
